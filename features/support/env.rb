cba01977ff5fadf1486e4f8ccd5144243c6b7acc
Luis Italo <luisitalomoreirasouza@gmail.com>
2022-09-09 10:35:47 -0300

    concluindo ambiente e relatorios

--- features/support/env.rb
+++ features/support/env.rb
@@ -5,40 +5,41 @@ require 'selenium-webdriver'
 require 'site_prism'
 require 'ostruct'
 require_relative 'page_helper.rb'
+require_relative 'helper.rb'
 
-BROWSER = ENV['BROWSER']
-AMBIENTE = ENV['AMBIENTE'] #homolog
-
-CONFIG = YAML.load_file(File.dirname(__FILE__) + "/ambientes/#{AMBIENTE}.yml" ) #homolog.yml
-
+BROWSERS = ENV['BROWSERS']
+AMBIENTE = ENV['AMBIENTE']
+CONFIG = YAML.load_file(File.dirname(__FILE__) + "/ambientes/#{AMBIENTE}.yml")
 
 World(Pages)
+World(Helper)
 
 Capybara.register_driver :selenium do |app|
-  if BROWSER.eql?('chrome')
-    Capybara::Selenium::Driver.new(app, :browser => :chrome)
-  
-  # elsif BROWSER.eql?('chrome_headless')
-  #   Capybara::Selenium::Driver.new(app, :browser => :chrome,
-  #   desired_capabilities: Selenium::WebDriver::Remote::Capabilities.chrome(
-  #    'chromeOptions' => {'args' =>['--headless', 'disable-gpu']}
-  #   )
-  #   )
-  elsif BROWSER.eql?('firefox')
-    Capybara::Selenium::Driver.new(app, :browser => :firefox, :marionette => true)
-
-  # elsif BROWSER.eql?('firefox_headless')
-  #   browser_options = Selenium::WebDriver::Firefox::Options.new(args: ['--headless'])
-  #   Capybara::Selenium::Driver.new(app, :browser => :firefox, options: browser_options)
-
+  if BROWSERS.eql?('chrome')
+    Capybara::Selenium::Driver.new(app,browser: :chrome)
     
+  # elsif BROWSERS.eql?('chrome_headless')
+  #   Capybara::Selenium::Driver.new(
+  #     app, browser: :chrome,
+  #     desired_capabilities: Selenium::WebDriver::Remote::Capabilities.chrome(
+  #       'chromeOptions' => { 'args' => ['headless', 'disable-gpu'] }
+  #     )
+  #   )
+  elsif BROWSERS.eql?('firefox')
+    Capybara::Selenium::Driver.new(app,browser: :firefox, marionette: true)
+
+  # elsif BROWSERS.eql?('firefox_headless')
+  #   browser_options = Selenium::WebDriver::Firefox::Options.new(args: ['--headless'])
+  #     Capybara::Selenium::Driver.new(
+  #       app,
+  #       browser: :firefox,
+  #       options: browser_options
+  #     )
   end
 end
 
-
-
 Capybara.configure do |config|
-    config.default_driver = :selenium #selenium_chrome #selenium_chrome_headless
-    config.app_host = CONFIG['url_padrao']
-    config.default_max_wait_time = 5
-  end
+  config.default_driver = :selenium
+  config.app_host = CONFIG['url_padrao']
+  config.default_max_wait_time = 5
+end
